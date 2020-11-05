e2f7d0d93742133cee8fe2c8dda2e1a163a93132
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-11-05 20:48:31 +0000

    refactored to improve modularity

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,25 @@
 const request = require("request");
 
-const query = process.argv[2];
 
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${query}`, function(error, response, body) {
-  if (error) {
-    console.log('Error:', error);
-    throw new Error(error);
-  } // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  const data = JSON.parse(body);
-  if (!data[0]) {
-    console.log(`No breeds found matching your query. Try again.`);
-  } else {
-    console.log(data[0].description);
-  }
-});
+
+const fetchBreedDescription = (breed, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+    if (!error) {
+      const data = JSON.parse(body);
+      if (data[0]) {
+        const description = data[0].description;
+        callback(null, description);
+      } else {
+        console.log(`Sorry. That breed isn't in our database.`);
+      }
+    } else {
+      callback(error, null);
+    } // Print the error if one occurred
+  });
+};
+
+
+
+
+module.exports = {fetchBreedDescription};
